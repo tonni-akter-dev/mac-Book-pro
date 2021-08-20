351cec19b1cae5d297fb36e382f11997d59a01bd
tonni276 <tanniakhter123@gmail.com>
2021-08-21 02:11:25 +0600

    third added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function getValue(productId) {
-    document.getElementById('default' + productId).innerText =0;
+    document.getElementById('default' + productId).innerText = 0;
+    totalUpdatePrice();
+}
+function getUpdatePrice(productId , price) {
+    document.getElementById('default' + productId).innerText = price;
+    totalUpdatePrice();
 }
 
 
@@ -7,38 +12,52 @@ function getValue(productId) {
 document.getElementById('default-memory-cost').addEventListener('click', function () {
     getValue('Memory');
 });
+
+document.getElementById('updateMemory').addEventListener('click', function () {
+    getUpdatePrice('Memory',180);
+  });
+// ........................storage button................
 document.getElementById('default-storage-cost').addEventListener('click' ,function() {
     getValue('Storage');
-});
-document.getElementById('default-delivery-cost').addEventListener('click', function () {
-    getValue('Delivery');
-});
-// update  memory storage and  delivery
-function getUpdatePrice(productId , price) {
-    document.getElementById('default' + productId).innerText = price;
-    totalUpdatePrice();
-}
-document.getElementById('updateMemory').addEventListener('click', function () {
-  getUpdatePrice('Memory',180);
 });
 document.getElementById('updateStorage').addEventListener('click' ,function() {
     getUpdatePrice('Storage',100);
 });
-document.getElementById('updateDelivery').addEventListener('click' ,function() {
-   getUpdatePrice('Delivery' , 20);
-});
-
-// storage 3rd button............
 document.getElementById('updateMoreStorage').addEventListener('click' ,function() {
     getUpdatePrice('Storage',180);
 });
+// delivery button..............................
+document.getElementById('default-delivery-cost').addEventListener('click', function () {
+    getValue('Delivery');
+});
+document.getElementById('updateDelivery').addEventListener('click' ,function() {
+    getUpdatePrice('Delivery' , 20);
+ });
 
 // total price ...............
-function totalUpdatePrice() {
+function totalUpdatePrice(isAdd) {
     const totalPrice = document.getElementById('totalPrice');
-    const defaultPrice = parseInt(document.getElementById('defaultCost').innerText);
-    const memoryPrice = parseInt(document.getElementById('defaultMemory').innerText);
+const defaultPrice = parseInt(document.getElementById('defaultCost').innerText);
+const memoryPrice = parseInt(document.getElementById('defaultMemory').innerText);
     const storagePrice = parseInt(document.getElementById('defaultStorage').innerText);
     const deliveryCost = parseInt(document.getElementById('defaultDelivery').innerText);
-    totalPrice.innerText = defaultPrice + memoryPrice + storagePrice + deliveryCost;
+        totalPrice.innerText = defaultPrice + memoryPrice + storagePrice + deliveryCost;
+    let finalTotal = parseInt(document.getElementById('final-total').innerText = totalPrice.innerText);
+    return finalTotal;
 }
+
+// cupon code part here............................
+document.getElementById('apply').addEventListener('click', function () {
+    const getInputValue = document.getElementById('promo-code');
+    const getInputText = getInputValue.value;
+    // console.log(getInputText);
+    if (getInputText == 'stevekaku') {
+        console.log('hi');
+          let finalTotal = finalTotal / 20;
+    }
+    else {
+   alert ('get promo code or pay total price')
+        }
+
+
+    });
